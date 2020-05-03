c4ad57c29375612865b8b3afb087069f7722c5d4
Younes Abdennadher <younes.abdennadher@etu.univ-cotedazur.fr>
2020-05-04 00:08:23 +0200

    #124 CHanged drones status

--- src/main/java/fr/polytech/entities/DroneStatus.java
+++ src/main/java/fr/polytech/entities/DroneStatus.java
@@ -1,5 +1,8 @@
 package fr.polytech.entities;
 
 public enum DroneStatus {
-    ON_CHARGE, ON_REPAIR, ON_DELIVERY, AVAILABLE, BACK_FROM_DELIVERY,;
+    ON_CHARGE,
+    ON_REPAIR,
+    ON_DELIVERY,
+    AVAILABLE
 }
